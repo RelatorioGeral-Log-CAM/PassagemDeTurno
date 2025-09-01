5caf9baf9d553d4db295af2e278ac20f40926188
Enzo Tomas Seixas Gomes Mota <enzo.tomas@grupoboticario.com.br>
2025-09-01 11:38:00 -0300

    ajustes vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   // Adicione esta linha com o nome do seu repositório
   base: '/PassagemDeTurno/', 
-  server: {
-    host: "::",
-    port: 8080,
-  },
+
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
